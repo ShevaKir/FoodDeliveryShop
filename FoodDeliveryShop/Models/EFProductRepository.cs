a027518c26274be786e633e7d4b161639dd98402
Kirill Shevchenko <kir971@gmail.com>
2022-03-10 02:04:57 +0200

    Use EF Core

---
+++ FoodDeliveryShop/Models/EFProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+
+namespace FoodDeliveryShop.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext _context;
+
+        public EFProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> Products => _context.Products;
+    }
+}
--- FoodDeliveryShop/Startup.cs
+++ FoodDeliveryShop/Startup.cs
@@ -1,38 +1,57 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using FoodDeliveryShop.Models;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
-using FoodDeliveryShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
 
 namespace FoodDeliveryShop
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration["Data:FoodDeliveryShopProducts:ConnectionString"]));
+            
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvc(routes => 
+
+
+            app.UseMvc(routes =>
             {
                 routes.MapRoute(name: "default", template: "{Controller=Product}/{action=List}/{id?}");
             });
+            SeedData.EnsurePopulated(app);
 
+            //app.UseEndpoints(endpoints => 
+            //{ 
+            //    endpoints.MapControllerRoute(name: "default", pattern: "{controller}/{action}/{id?}");
+            //});
             //if (env.IsDevelopment())
             //{
             //    app.UseDeveloperExceptionPage();
